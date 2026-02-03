d5db41e3dd5c2465c72e58d898885b2be3f7b56d
nomi <=>
2026-02-03 15:58:20 +0900

    복구

--- boards/models.py
+++ boards/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.conf import settings
 # Create your models here.
 
-
 class Master(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
