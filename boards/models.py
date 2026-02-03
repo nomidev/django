59ba4ff60934d05a0526239fc50d3d68fadedacd
nomi <=>
2026-02-03 15:54:15 +0900

    test

--- boards/models.py
+++ boards/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 # Create your models here.
 
+
 class Master(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
