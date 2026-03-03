da4079f1b66853e0115027155464e2088681a696
nomi <nomisoul@gmail.com>
2026-03-03 17:00:57 +0900

    like 추가

--- boards/models.py
+++ boards/models.py
@@ -20,6 +20,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='board_posts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='board_likes', blank=True)
 
     def __str__(self):
         return self.title
@@ -27,8 +28,13 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='board_comments')
+    parent = models.ForeignKey('self', related_name='replies', null=True, blank=True, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='board_comment_likes', blank=True)
 
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'    
+
+    def children(self):
+        return Comment.objects.filter(parent=self)
