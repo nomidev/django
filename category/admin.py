778487f966189aaa094449c2c40bed18ee70ee1e
nomi <nomisoul@gmail.com>
2026-03-13 17:59:33 +0900

    menuAdmin 수정

--- category/admin.py
+++ category/admin.py
@@ -5,11 +5,14 @@ from common.models import Code
 # Register your models here.
 # admin.site.register(Menu)
 
+# URL 유형에 해당하는 CodeType의 ID (예시)
+URL_TYPE_CODE_ID = 1
+
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
     # db_field: 필드 객체, request: 현재 요청 객체
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "url_type":  # 필터링할 필드 이름 확인
             # 특정 조건(예: code가 'A'로 시작하는 것만)에 맞는 QuerySet 설정
-            kwargs["queryset"] = Code.objects.filter(code_type_id=1)
+            kwargs["queryset"] = Code.objects.filter(code_type_id=URL_TYPE_CODE_ID)
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
